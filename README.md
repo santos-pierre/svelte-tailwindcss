6a6c352319336d2218640eb13c81750d3402100d
Santos Pierre <santospierre1305@gmail.com>
2022-02-17 17:59:26 +0100

    First Commit :tada:

---
+++ README.md
@@ -0,0 +1,31 @@
+# Svelte + Tailwind :heart_eyes:
+
+## Vite Installation - Svelte
+
+Vite Setup
+
+```bash
+npm create vite@latest
+
+npm install
+```
+
+1. Name Your Project.
+2. Choose Svelte.
+3. Choose Svelte => Javascript / Svelte-TS => Typescript.
+
+## Tailwind CSS Installation
+
+Let Svelte do the work !
+
+```bash
+npm i svelte-add
+
+npx svelte-add tailwindcss
+
+npm install
+
+npm run dev
+```
+
+Enjoy :tada:
---
+++ src/App.svelte
@@ -0,0 +1,52 @@
+<script>
+  import logo from './assets/svelte.png'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <img src={logo} alt="Svelte Logo" />
+  <h1 class="text-[#ff3e00] text-uppercase text-[4rem] font-thin mt-8 mx-auto max-w-[14rem] md:max-w-none">Hello Svelte + Tailwind CSS !</h1>
+
+  <Counter />
+
+  <p>
+    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
+    apps.
+  </p>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
+    the officially supported framework, also powered by Vite!
+  </p>
+
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+
+  img {
+    height: 16rem;
+    width: 16rem;
+  }
+
+  p {
+    max-width: 14rem;
+    margin: 1rem auto;
+    line-height: 1.35;
+  }
+
+  @media (min-width: 480px) {
+    p {
+      max-width: none;
+    }
+  }
+</style>
